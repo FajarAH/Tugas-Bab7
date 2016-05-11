e828569057aba744f62364e328051f3307ce15e7
FajarAH <akbaransyah@gmail.com>
2016-05-11 13:28:49 +0700

    membuat atribut dan override method abstract

--- Laporan Bab 7/src/Premium.java
+++ Laporan Bab 7/src/Premium.java
@@ -1,6 +1,42 @@
 
 public class Premium extends Salon {
+    private double hargaJasa;
+    private double totalJasa;
+    private double hargaProduk;
+    private double totalProduk;
      public Premium(String nama) {
         super(nama);
     }
+     @Override
+     public void totalJasa(int pil) {
+        if (pil == 1) {
+            double c = super.getCreambath();
+            hargaJasa = c;
+            super.setCreambath(c);
+        } else if (pil == 2) {
+            double p = super.getPotong();
+            hargaJasa = p;
+            super.setPotong(p);
+        } else if(pil == 3) {
+            double l = super.getLulur();
+            hargaJasa = l;
+            super.setLulur(l);
+        }
+        totalJasa += hargaJasa;
+    }
+    @Override
+    public void totalProduk(int pil, int banyak) {
+        if (pil == 1) {
+            double s = super.getShampoo();
+            hargaProduk = s;
+        } else if (pil == 2) {
+            double sb = super.getSabun();
+            hargaProduk = sb;
+        } else if(pil == 3){
+            double m = super.getMasker();
+            hargaProduk = m;
+        }
+        hargaProduk = hargaProduk * banyak;
+        totalProduk += hargaProduk;
+    } 
 }
