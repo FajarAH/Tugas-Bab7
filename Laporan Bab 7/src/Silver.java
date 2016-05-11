031757b0f7aa97c86fbc68512e3751700d69ac88
FajarAH <akbaransyah@gmail.com>
2016-05-11 13:34:55 +0700

    Membuat atribut dan override method class Silver

--- Laporan Bab 7/src/Silver.java
+++ Laporan Bab 7/src/Silver.java
@@ -1,6 +1,42 @@
 
 public class Silver extends Salon {
+     private double hargaJasa;
+    private double totalJasa;
+    private double hargaProduk;
+    private double totalProduk;
       public Silver(String nama) {
         super(nama);
+    }
+         @Override
+    public void totalJasa(int pil) {
+        if (pil == 1) {
+            double c = super.getCreambath() - (super.getCreambath() * 0.1);
+            hargaJasa = c;
+            super.setCreambath(c);
+        } else if (pil == 2) {
+            double p = super.getPotong() - (super.getPotong() * 0.1);
+            hargaJasa = p;
+            super.setPotong(p);
+        } else if (pil ==3) {
+            double l = super.getLulur() - (super.getLulur() * 0.1);
+            hargaJasa = l;
+            super.setLulur(l);
+        }
+        totalJasa += hargaJasa;
+    }
+    @Override
+    public void totalProduk(int pil, int banyak) {
+        if (pil == 1) {
+            double s = super.getShampoo();
+            hargaProduk = s;
+        } else if (pil == 2) {
+            double sb = super.getSabun();
+            hargaProduk = sb;
+        } else if(pil == 3) {
+            double m = super.getMasker();
+            hargaProduk = m;
+        }
+        hargaProduk = hargaProduk * banyak;
+        totalProduk += hargaProduk;
     }
 }
