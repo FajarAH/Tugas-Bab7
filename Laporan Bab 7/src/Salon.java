d2e64732c45a075cf6e843c259cbcd89fb3f8d92
FajarAH <akbaransyah@gmail.com>
2016-05-11 13:17:27 +0700

    Membuat method set dan get

--- Laporan Bab 7/src/Salon.java
+++ Laporan Bab 7/src/Salon.java
@@ -13,5 +13,50 @@ public abstract class Salon {
     }
     public abstract void totalJasa(int pil);
     public abstract void totalProduk(int pil, int banyak);
+    public String getNama() {
+        return nama;
+    }
+    public double getJasa() {
+        return totaljasa;
+    }
+    public double getProduk() {
+        return totalproduk;
+    }
+    public void setCreambath(double c) {
+        creambath = c;
+    }
+    public void setPotong(double p) {
+        potong = p;
+    }
+    public void setLulur(double l) {
+        lulur = l;
+    }
+    public void setShampoo(double s) {
+        shampoo = s;
+    }
+    public void setSabun(double sb) {
+        sabun = sb;
+    }
+    public void setMasker(double m) {
+        masker = m;
+    }
+    public double getCreambath() {
+        return creambath;
+    }
+    public double getPotong() {
+        return potong;
+    }
+    public double getLulur() {
+        return lulur;
+    }
+    public double getShampoo() {
+        return shampoo;
+    }
+    public double getSabun() {
+        return sabun;
+    }
+    public double getMasker() {
+        return masker;
+    }
     
 }
