717950ed70d5c392a6b0dfeac321fc1d095a9520
FajarAH <akbaransyah@gmail.com>
2016-05-11 13:19:49 +0700

    membuat method untuk menampilkan jasa dan produk

--- Laporan Bab 7/src/Salon.java
+++ Laporan Bab 7/src/Salon.java
@@ -58,5 +58,26 @@ public abstract class Salon {
     public double getMasker() {
         return masker;
     }
-    
+     public void displayJasa() {
+        System.out.println("=====================================================");
+        System.out.println("\t DAFTAR LAYANAN SALON FORZA");
+        System.out.println("=====================================================");
+        System.out.printf("|%-5s |%-30s |%-11s | \n", "No", "LAYANAN", "Harga");
+        System.out.println("=====================================================");
+        System.out.printf("|%-5s |%-30s |%10s | \n", "1", "CREAMBATH ", "Rp.  50.000");
+        System.out.printf("|%-5s |%-30s |%10s | \n", "2", "POTONG", "Rp.  25.000");
+        System.out.printf("|%-5s |%-30s |%10s | \n", "3", "LULUR", "Rp. 100.000");
+        System.out.println("=====================================================");
+    }
+    public void displayProduk() {
+        System.out.println("=====================================================");
+        System.out.println("\t DAFTAR PRODUK SALON FORZA");
+        System.out.println("=====================================================");
+        System.out.printf("|%-5s |%-30s |%-11s | \n", "No", "PRODUK", "Harga");
+        System.out.println("=====================================================");
+        System.out.printf("|%-5s |%-30s |%10s | \n", "1", "SHAMPOO ", "Rp.  20.000");
+        System.out.printf("|%-5s |%-30s |%10s | \n", "2", "SABUN", "Rp.  15.000");
+        System.out.printf("|%-5s |%-30s |%10s | \n", "3", "MASKER", "Rp.  18.000");
+        System.out.println("=====================================================");
+    }
 }
