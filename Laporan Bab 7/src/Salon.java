e726016ee25284e5e21dc176c7a38cecbb322f6f
FajarAH <akbaransyah@gmail.com>
2016-05-11 13:10:28 +0700

    Membuat atribut dan constructor class Salon

--- Laporan Bab 7/src/Salon.java
+++ Laporan Bab 7/src/Salon.java
@@ -1,3 +1,15 @@
 public abstract class Salon {
+      private String nama;
+    private double totaljasa;
+    private double totalproduk;
+    private double creambath = 50000;
+    private double potong = 25000;
+    private double lulur = 100000;
+    private double shampoo = 20000;
+    private double sabun = 15000;
+    private double masker = 18000;
+    public Salon(String nama) {
+        this.nama = nama;
+    }
     
 }
